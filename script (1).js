2c46aef8280bad3cc4ad0428ed883d7799a8126a
Neala10 <mmorris317@ivytech.edu>
2025-04-09 19:31:26 -0400

    Update script (1).js

--- script (1).js
+++ script (1).js
@@ -4,17 +4,17 @@ const courseButton = document.getElementById('courseButton');
 const messageElement = document.getElementById('message');
 
 // Array of friendly courseings
-const courses = [
-    "INFM 109",
-    "SDEV 120"
-];
-
-// Click event handler
+const courses = [ "INFM 109", "SDEV 120"];
+    
+ // Click event handler  
 courseButton.addEventListener('click', function() {
     // Pick a random courseing
-    const randomIndex = Math.floor(Math.random() * courses.length);
+ const randomIndex = Math.floor(Math.random() * courses.length);
     const course = courses[randomIndex];
-    
-    // Display the course
+
+ // Display the course
     messageElement.textContent = course;
 });
+   
+    
+   
